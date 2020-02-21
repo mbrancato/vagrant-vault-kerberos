ac55f3d4befa2a6814d7a164a71e1f67d0345b7e
Mike Brancato <mike@mikebrancato.com>
2020-02-21 11:16:58 -0500

    Add web app request / response output

--- FancyApp/Pages/Index.cshtml.cs
+++ FancyApp/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,36 +9,46 @@ using Microsoft.Extensions.Logging;
 
 namespace FancyApp.Pages
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    public string RequestMsg { get; set; }
+    public string ResponseMsg { get; set; }
+    public string ResponseContent { get; set; }
+
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-          var host = "http://vault.domain.local:8200/v1/auth/kerberos/domain.local/login";
-          var handler = new HttpClientHandler
-          {
-              UseDefaultCredentials = true,
-              AllowAutoRedirect = true,
-          };
-          
-          using (var client = new HttpClient())
-          {
-              var req = new HttpRequestMessage(HttpMethod.Post, host);
-              req.Credentials      = System.Net.CredentialCache.DefaultNetworkCredentials;
-
-              var res = client.SendAsync(req);
-              var responseResult = res.Result;
-              RequestMsg = req.ToString();
-              ResponseMsg = res.Result.ToString();
-          }
-
-        }
+      _logger = logger;
     }
+
+    public void OnGet()
+    {
+      var host = "http://vault.domain.local:8200/v1/auth/kerberos/domain.local/login";
+      var handler = new HttpClientHandler
+      {
+        UseDefaultCredentials = true,
+        AllowAutoRedirect = true,
+      };
+
+      using (var client = new HttpClient(handler))
+      {
+        var req = new HttpRequestMessage(HttpMethod.Post, host);
+        //req.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+
+        //var res = client.SendAsync(req);
+        //var responseResult = res.Result;
+        RequestMsg = req.ToString();
+        using (HttpResponseMessage response = client.SendAsync(req).Result)
+        {
+          ResponseMsg = response.ToString();
+          using (HttpContent content = response.Content)
+          {
+            var json = content.ReadAsStringAsync().Result;
+            ResponseContent = json;
+          }
+        }
+      }
+    }
+  }
 }
